b58fb2b7b841505c0535a7cfba4fb6a3765db3f9
The Platform Authors <platform@entilda.com>
2022-10-01 05:38:51 +0200

    add minor changes to satellite app

--- sandbox/system/applications/satellite/index.php
+++ sandbox/system/applications/satellite/index.php
@@ -6,14 +6,55 @@
         * {
             padding: 0px;
             margin: 0px;
+            overflow: hidden;
         }
 
         body {
             overflow: hidden;
+            background: #000;
+        }
+
+        #logo {
+            background: #000;
+            width: 256px;
+            height: 256px;
+            border: 10px solid white;
+            border-radius: 50%;
+            position: absolute;
+            z-index: 99;
+        }
+
+        #globe {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        }
+
+        #nav {
+            background: #000;
+            width: 100vw;
+            height: 8vh;
+            position: absolute;
+            top: 0;
+            z-index: 9;
+        }
+
+        #status {
+            background: #000;
+            width: 100vw;
+            height: 8vh;
+            position: absolute; 
+            bottom: 0;  
+            z-index: 9;
         }
     </style>
 </head>
 <body>
-<iframe width="100%" height="1096" src="https://www.youtube.com/embed/E5WpblyBR38" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<div id="logo">
+    <iframe id="globe" src="globe/index.html"></iframe>
+</div>
+<div id="nav"></div>
+<iframe id="feed" width="100%" height="1096" src="https://www.youtube.com/embed/Y1qQZbTF8iQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<div id="status"></div>
 </body>
 </html>
