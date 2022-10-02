1b90f04ade2d41b5730c76480553ff0076e61d02
The Platform Authors <platform@entilda.com>
2022-10-02 21:05:14 +0200

    fix ui issues

--- sandbox/system/applications/satellite/assets/js/app.js
+++ sandbox/system/applications/satellite/assets/js/app.js
@@ -1,24 +1,57 @@
 $(function(){
-    var t = new Date();
     var clip = {
         "default": "./globe/assets/clips/earth.mp4",
         "day": "./globe/assets/clips/earth-only.mp4",
         "night": "./globe/assets/clips/earth-night.mp4",
     }
 
-    h = t.getHours();
-    $('#logo').click(function(){
+    $('#radar').click(function(){
         $('.camera').toggle();
     });
 
-    function isNight(){
-      return (h >= 18 || h <= 6) ? true : false;
+    $('#satellite').click(function(){
+        $('.camera.screen1').css('display', 'block');
+        $('.camera.screen2').css('display', 'none');
+    });
+
+    $('#iss-satellite').click(function(){
+        $('.camera.screen1').css('display', 'none');
+        $('.camera.screen2').css('display', 'block');
+    });
+
+    letThereBeLight();
+    setInterval(function() {
+        letThereBeLight();
+    }, 60000);
+
+    function isNight(h){
+        return (h >= 18 || h <= 5) ? true : false;
     }
 
-    if (isNight()){
-        $('video').attr('src', clip.night)
-    } else {
-        $('video').attr('src', clip.day)
+    function isDay(h){
+        return (h >= 6 || h <= 11) ? true : false;
     }
+
+    function isSunnyDay(h)
+    {
+        return (h >= 12 || h <= 17) ? true : false;
+    }
+    
+    function letThereBeLight(){
+        var t = new Date();
+        var h = t.getHours();
+        switch(true){
+            case isDay(h):
+                $('video').attr('src', clip.day)
+                break;
+            case isNight(h):
+                $('video').attr('src', clip.night)
+                break;
+            case isSunnyDay(h):
+                $('video').attr('src', clip.default)
+                break;
+        }
+    }
+    
     
 })
--- sandbox/system/applications/satellite/index.php
+++ sandbox/system/applications/satellite/index.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
 </head>
 <body>
-<a id="logo" href="javascript:{}">
+<a id="radar" href="javascript:{}">
     <video src="./globe/assets/clips/earth-night.mp4" autoplay loop>
         <source src="./globe/assets/clips/earth-night.mp4" type="video/mp4"/>  		
 		Your Browser does not support the video element
@@ -18,15 +18,28 @@
 </a>
 <div id="nav">
     <div id="nav-controls">
-        <div class="control datetime">
-            <span id="datetime" class="current date"></span> &nbsp; 
-            <span id="time" class="current time"></span>
+        <a id="shuttle" href="javascript:{}">
+            <div class="control shuttle"></div>
+        </a>
+        <a id="satellite" href="javascript:{}">
+            <div class="control satellite"></div>
+        </a> &nbsp;
+        <a id="iss-satellite" href="javascript:{}">
+            <div class="control iss-satellite"></div>
+        </a>
+        <span id="datetime" class="current date">
+            <div class="control datetime"></div>
+        </span>
+        <span id="time" class="current time">
+            <div class="control time"></div>
+        </span>
         </div>
     </div>
 </div>
 <iframe class="camera screen1" width="100%" height="1096" src="https://www.youtube.com/embed/86YLFOog4GM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <iframe class="camera screen2" width="100%" height="1096" src="https://www.youtube.com/embed/Y1qQZbTF8iQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <div id="status"></div>
+<script type="text/javascript" src="https://code.responsivevoice.org/responsivevoice.js?key=5znGKiaR"></script>
 <script type="text/javascript" src="./assets/js/jquery/3.0.0/jquery.min.js"></script>
 <script type="text/javascript" src="./assets/js/time.js"></script>
 <script type="text/javascript" src="./assets/js/app.js"></script>
