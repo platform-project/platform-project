2832adf92a38705b4e5afb0afb853bb13d96501e
The Platform Authors <platform@entilda.com>
2022-10-06 03:38:06 +0200

    updated few apps

---
+++ sandbox/workspace/bookmarks/s2maps/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * s2maps
+ * Bookmark link to URL
+ *
+ * @version     : index.php 40 2011-02-09 14:10:00Z biyi $
+ * @package     Platform
+ * @category    Bookmarks
+ * @author      Biyi Akinpelu
+ * @link        mailto:biyi@entilda.com
+ * @copyright   Copyright (C) 2011 Entilda IT Solutions. All rights reserved.
+ * @license     GNU/GPL, see LICENSE.php
+ * Platform is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// initilizing platform for self-contained objects
+platform_launch_initialize();
+
+redirect_to('https://s2maps.eu');
+?>
