a888f86e9b0ff5fb4ccaf543467e458f1a3f7929
The Platform Authors <platform@entilda.com>
2022-10-07 08:44:38 +0200

    added solarsystem 3d model app

---
+++ sandbox/system/applications/solarsystem/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Solar System</title>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+#solarsystem {
+    position: absolute;
+    top: -50px;
+}
+</style>
+</head>
+<body>
+<div id="solarsystem" class="sketchfab-embed-wrapper"> <iframe title="Solar System" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="1920" height="1080" src="https://sketchfab.com/models/00c2fd1b0cf74846b540c3c4b95e7f6b/embed?autospin=1&autostart=1&preload=1&ui_theme=dark&dnt=1"> </iframe> </div>
+</body>
+</html>
