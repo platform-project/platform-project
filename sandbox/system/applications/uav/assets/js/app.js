3dd688c941f41427c91c030262ae3beacadd9f0e
The Platform Authors <platform@entilda.com>
2022-12-03 17:35:26 +0200

    added UAV

---
+++ sandbox/system/applications/uav/assets/js/app.js
@@ -0,0 +1,43 @@
+$(function(){
+
+    $('.uav.uav-0').click(function(){
+        $('#uav').attr('src', 'uav-0.html');
+    });
+
+    $('.uav.uav-1').click(function(){
+        $('#uav').attr('src', 'uav-1.html');
+    });
+
+    $('.uav.uav-2').click(function(){
+        $('#uav').attr('src', 'uav-2.html');
+    });
+
+    $('.uav.uav-3').click(function(){
+        $('#uav').attr('src', 'uav-3.html');
+    });
+
+    $('.uav.uav-4').click(function(){
+        $('#uav').attr('src', 'uav-4.html');
+    });
+
+    $('.uav.uav-5').click(function(){
+        $('#uav').attr('src', 'uav-5.html');
+    });
+
+    $('.uav.uav-6').click(function(){
+        $('#uav').attr('src', 'uav-6.html');
+    });
+
+    $('.uav.uav-7').click(function(){
+        $('#uav').attr('src', 'uav-7.html');
+    });
+
+    $('.uav.uav-8').click(function(){
+        $('#uav').attr('src', 'uav-8.html');
+    });
+
+    $('.uav.uav-9').click(function(){
+        $('#uav').attr('src', 'uav-9.html');
+    });
+
+});
