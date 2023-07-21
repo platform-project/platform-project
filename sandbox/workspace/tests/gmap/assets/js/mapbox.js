369064b634687ca6a30f159720eeecccce73e671
The Platform Authors <platform@entilda.com>
2023-07-21 22:52:30 +0200

    gmap ui enhancement

--- sandbox/workspace/tests/gmap/assets/js/mapbox.js
+++ sandbox/workspace/tests/gmap/assets/js/mapbox.js
@@ -2,6 +2,57 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYmlsZWNrbWUiLCJhIjoiY2xpZnNqaGF0MDNmcjNqcDU2b
 
 navigator.geolocation.getCurrentPosition( successLocation, errorLocation, { enableHighAccuracy: true });
 
+/* Given a query in the form "lng, lat" or "lat, lng"
+* returns the matching geographic coordinate(s)
+* as search results in carmen geojson format,
+* https://github.com/mapbox/carmen/blob/master/carmen-geojson.md */
+const coordinatesGeocoder = function (query) {
+    // Match anything which looks like
+    // decimal degrees coordinate pair.
+    const matches = query.match(
+    /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+    );
+    if (!matches) {
+        return null;
+    }
+     
+    function coordinateFeature(lng, lat) {
+        return {
+            center: [lng, lat],
+            geometry: {
+                type: 'Point',
+                coordinates: [lng, lat]
+            },
+            place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+            place_type: ['coordinate'],
+            properties: {},
+            type: 'Feature'
+        };
+    }
+     
+    const coord1 = Number(matches[1]);
+    const coord2 = Number(matches[2]);
+    const geocodes = [];
+     
+    if (coord1 < -90 || coord1 > 90) {
+        // must be lng, lat
+        geocodes.push(coordinateFeature(coord1, coord2));
+    }
+     
+    if (coord2 < -90 || coord2 > 90) {
+        // must be lat, lng
+        geocodes.push(coordinateFeature(coord2, coord1));
+    }
+     
+    if (geocodes.length === 0) {
+        // else could be either lng, lat or lat, lng
+        geocodes.push(coordinateFeature(coord1, coord2));
+        geocodes.push(coordinateFeature(coord2, coord1));
+    }
+     
+        return geocodes;
+};
+
 function successLocation(position){
     setUpMap([position.coords.longitude, position.coords.latitude]);
 }
@@ -28,6 +79,17 @@ function setUpMap(center){
         'top-left'
     );
 
+    map_satellite.addControl(
+        new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                localGeocoder: coordinatesGeocoder,
+                zoom: 17,
+                placeholder: '',
+                mapboxgl: mapboxgl,
+                reverseGeocode: true
+            })
+        );
+
     // streets view
     var map_street = new mapboxgl.Map({
         container: 'map_street',
@@ -44,6 +106,17 @@ function setUpMap(center){
         'top-left'
     );
 
+    map_street.addControl(
+        new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                localGeocoder: coordinatesGeocoder,
+                zoom: 17,
+                placeholder: '',
+                mapboxgl: mapboxgl,
+                reverseGeocode: true
+            })
+        );
+
     // night view
     var map_night = new mapboxgl.Map({
         container: 'map_night',
@@ -59,4 +132,15 @@ function setUpMap(center){
         }), 
         'top-left'
     );
+
+    map_night.addControl(
+        new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                localGeocoder: coordinatesGeocoder,
+                zoom: 17,
+                placeholder: '',
+                mapboxgl: mapboxgl,
+                reverseGeocode: true
+            })
+        );
 }
