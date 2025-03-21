e1d69e49cfa57af7ae2af513276624af519da410
Biyi Akinpelu <bileckme@gmail.com>
2025-03-21 14:25:30 +0200

    fix ebooks app ui

--- sandbox/system/scripts/pdf2jpg.sh
+++ sandbox/system/scripts/pdf2jpg.sh
@@ -6,13 +6,13 @@ JPG_PATH="$BASE_PATH/assets/thumbnails" #"$2"
 sudo -k
 process_pdf(){
     cd $PDF_PATH &&
-    find * -name '*.pdf' -exec /usr/bin/gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r144 -sPageList=1 -sOutputFile={}.jpg {} \;
+    find -L * -name '*.pdf' -exec /usr/bin/gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r144 -sPageList=1 -sOutputFile={}.jpg {} \;
 }
 
 process_jpg(){
     cd $PDF_PATH &&
-    find * -name '*.jpg' -exec cp -var {} $JPG_PATH \;
-    find * -name '*.jpg' -exec rm -f {} \;
+    find -L * -name '*.jpg' -exec cp -var {} $JPG_PATH \;
+    find -L * -name '*.jpg' -exec rm -f {} \;
 }
 
 process_pdf
